f181ec6239eb12ef2b9bf93ca906b92e6ea60089
michelle jasmin coffin-taylor <845860590@kent-202-17m.local>
2017-05-16 10:02:31 +1200

    Completed Javascript Functions

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Functions
+
+function Hello(){
+	console.log("Hello");
+}
+
+Hello();
+Hello();
+
+function WhatisMyName(name){
+	console.log("Your name is " + name);
+}
+
+WhatisMyName("Michelle");
+WhatisMyName("Jack");
